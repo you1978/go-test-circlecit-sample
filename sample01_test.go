2967b3f09058711c78fb8fbeb00f54b18de87fdf
trinity <tanaka.yohei@blueship.co.jp>
2020-04-14 11:10:47 +0800

    test

--- sample01_test.go
+++ sample01_test.go
@@ -1,13 +1,13 @@
 package sample01
 
-import {
- "testing"
-}
+import (
+  "testing"
+)
 
-func TestHelloWorld(t *testing.T){
- actual := HelloWorld("hoge")
- expected := "hello World, hoge"
- if actual != expected {
-	t.Errorf("actual %v\nwant %v", actual, expected)
- }
+func TestHelloWorld(t *testing.T) {
+  actual := HelloWorld("hoge")
+  expected := "hello world, hoge"
+  if actual != expected {
+    t.Errorf("actual %v\nwant %v", actual, expected)
+  }
 }
